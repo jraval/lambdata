5609fb678cac72fe57247bfafb8ac2f9c7479c9c
theraval <theraval@gmail.com>
2021-03-19 19:32:09 -0400

    Prepare to distribute our package

--- mylambdata/hello.py
+++ mylambdata/hello.py
@@ -1,6 +1,11 @@
 #TODO: import enlarge
+from pandas import DataFrame
 
 from mylambdata.mymod import enlarge
 
 print("HELLO")
+
 print(enlarge(8))
+
+df = DataFrame({"state": ["CT", "CO", "CA", "TX"]})
+print(df.head())
---
+++ setup.py
@@ -0,0 +1,20 @@
+ setup.py file
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="lambdata", # the name that you will install via pip
+    version="1.0",
+    author="Jai R",
+    author_email="raval-jay@lambdastudents.com",
+    description="A short description",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    #license="MIT",
+    url="https://github.com/jraval/lambdata",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
